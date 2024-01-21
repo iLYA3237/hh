90c087742ebb31bb565877d6e8f3a727cae4d297
yangxijia <111684520+yangxijia@users.noreply.github.com>
2024-01-21 11:34:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def get_birthday():
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
+ 
     return get_words()
   return words.json()['data']['text']
 
